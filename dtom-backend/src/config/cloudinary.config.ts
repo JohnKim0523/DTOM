da039cfdd79216011f1557ba6e978498bc8b6896
John Kim <johnkim052304@gmail.com>
2026-02-12 18:23:25 -0500

    remove hardcoded cloudinary credentials, use env vars only

--- dtom-backend/src/config/cloudinary.config.ts
+++ dtom-backend/src/config/cloudinary.config.ts
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'ddgxzk4pf',
-  api_key: process.env.CLOUDINARY_API_KEY || '627184227971789',
-  api_secret: process.env.CLOUDINARY_API_SECRET || '8Hfg1rZdMkMP6SOcgxC7HvmKCZk',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export const avatarStorage = new CloudinaryStorage({
